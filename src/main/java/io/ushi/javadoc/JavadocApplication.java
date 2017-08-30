eee824afabd620db242dcc5c45bd060dc1f01543
zhouleibo <zhouleibo@qjdchina.com>
2017-08-30 17:34:04 +0800

    init

---
+++ src/main/java/io/ushi/javadoc/JavadocApplication.java
@@ -0,0 +1,12 @@
+package io.ushi.javadoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavadocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavadocApplication.class, args);
+	}
+}
---
+++ src/main/java/io/ushi/javadoc/controller/DocController.java
@@ -0,0 +1,39 @@
+package io.ushi.javadoc.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Doc主页控制类
+ *
+ * Created by zhouleibo on 2017/8/30.
+ */
+@RestController
+@RequestMapping("/doc")
+public class DocController {
+
+    @RequestMapping(value = "/group/{id}/artifacts", method = RequestMethod.GET)
+    public List<String> artifacts(@PathVariable("id") String groupId) {
+
+        List<String> g1 = new ArrayList<String>();
+        g1.add("A");
+        g1.add("B");
+
+        List<String> g2 = new ArrayList<String>();
+        g2.add("C");
+        g2.add("D");
+
+        Map<String, List<String>> mock = new HashMap<String, List<String>>();
+        mock.put("g1", g1);
+        mock.put("g2", g2);
+
+        return mock.get(groupId);
+    }
+}
